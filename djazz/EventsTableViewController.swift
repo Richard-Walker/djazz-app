d90bdde45d6e9b753ccd6d0908cf7cb4c47fd9eb
Richard Walker <rwa@emakina.com>
2015-02-15 20:25:08 +0100

    UI ready for first story!

---
+++ djazz/EventsTableViewController.swift
@@ -0,0 +1,114 @@
+//
+//  EventsTableViewController.swift
+//  djazz
+//
+//  Created by Richard Walker on 15/02/15.
+//  Copyright (c) 2015 Bright Moods Studios. All rights reserved.
+//
+
+import UIKit
+
+class EventsTableViewController: UITableViewController {
+    let eventNames = ["Wake-up breeze", "Atomic warning", "Super cool event"]
+    let eventTimes = ["07:30", "09:30", "16:45"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+        
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        // #warning Potentially incomplete method implementation.
+        // Return the number of sections.
+        return 1
+    }
+
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete method implementation.
+        // Return the number of rows in the section.
+        return eventNames.count
+    }
+
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("EventCellPrototype", forIndexPath: indexPath) as EventsTableCell
+
+        // Configure the cell...
+        
+        cell.eventNameLabel.text = eventNames[indexPath.row]
+        cell.eventTimeLabel.text = eventTimes[indexPath.row]
+        cell.eventProgressView.progress = 0
+        
+        if indexPath.row == 0 {
+            cell.eventProgressView.progress = 0.6
+            cell.eventTimeLabel.hidden = true
+            cell.eventEditButton.hidden = true
+            cell.eventSwitch.hidden = true
+            cell.eventStopButton.hidden = false
+            cell.eventActivityIndicator.startAnimating()
+            
+        }
+
+        return cell
+    }    
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+        // Return NO if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+        if editingStyle == .Delete {
+            // Delete the row from the data source
+            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
+        } else if editingStyle == .Insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+        // Return NO if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using [segue destinationViewController].
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
